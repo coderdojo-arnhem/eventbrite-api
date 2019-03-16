914ea11ecb4f2f35c72197d756ba2c33b82efff1
Tim Schlechter <timschlechter@gmail.com>
2019-03-16 20:56:31 +0100

    Customer templates for CoderDojo Arnhem

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -3,23 +3,25 @@
     <main id="main" class="site-main" role="main">
         <?php
             // Set up and call our Eventbrite query.
-            $events = new Eventbrite_Query(apply_filters('eventbrite_query_args', array(
+            $events = new Eventbrite_Query( apply_filters( 'eventbrite_query_args', array(
                 'status' => 'live',
                 'display_private' => true
-            )));
+            ) ) );
 
-            if ($events->have_posts()) :
+            if ( $events->have_posts() ) :
                 $first = true;
-                while ($events->have_posts()) :
+                while ( $events->have_posts() ) : 
                     $events->the_post();
+                    $venue = eventbrite_event_venue();
                     
-                    if ($first):
+                    if ( $first ):
                         $first = false;
                         ?>
                         <article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
                             <header class="entry-header">
-                                <h1 class="article-h1 center">Komende editie</h1>
+                                <h1 class="article-h1 center">Komende editie op <?= mysql2date('j F Y', eventbrite_event_start()->local) ?></h1>
                             </header>
+                            <address class="center"><a href="https://www.google.com/maps/search/<?= $venue->name ?> <?= $venue->address->city ?>" target="_blank"><?= $venue->name ?></a>, <?= $venue->address->localized_address_display ?></address>
                             <div class="entry-content">
                                 <?php eventbrite_ticket_form_widget(); ?>
                                 <?php the_content(); ?>
@@ -30,13 +32,13 @@
                         <?php
                     else:
                         ?>
-						<h4><?php echo eventbrite_event_time(); ?>, <a href="?eventbrite_id=<?php the_ID(); ?>"><?php the_title(); ?></a></h4>
+						<h4><?= mysql2date('j F', eventbrite_event_start()->local) ?>: <a href="?eventbrite_id=<?php the_ID(); ?>"><?php the_title(); ?></a></h4>
                         <?php
                     endif;
-                endwhile;
+                endwhile;         
             else :
                 // If no content, include the "No posts found" template.
-                get_template_part('content', 'none');
+                get_template_part( 'content', 'none' );
 
             endif;
 
--- tmpl/eventbrite-single.php
+++ tmpl/eventbrite-single.php
@@ -1,18 +1,20 @@
 <?php
 // Get our event based on the ID passed by query variable.
-$event = new Eventbrite_Query(array( 'p' => get_query_var('eventbrite_id') ));
+$event = new Eventbrite_Query( array( 'p' => get_query_var( 'eventbrite_id' ) ) );
 
-if ($event->have_posts()) :
-    while ($event->have_posts()) :
+if ( $event->have_posts() ) :
+    while ( $event->have_posts() ) : 
         $event->the_post();
         get_header();
-        ?>
+        $venue = eventbrite_event_venue();
+		?>
 	    <div id="primary" class="content-area">
 		    <main id="main" class="site-main" role="main">
                 <article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <header class="entry-header">
-                        <h1 class="article-h1 center"><?php the_title(); ?></h1>
+                        <h1 class="article-h1 center"><?= mysql2date('j F Y', eventbrite_event_start()->local) ?>: <?php the_title(); ?></h1>
                     </header>
+                    <address class="center"><a href="https://www.google.com/maps/search/<?= $venue->name ?> <?= $venue->address->city ?>" target="_blank"><?= $venue->name ?></a>, <?= $venue->address->localized_address_display ?></address>
                     <div class="entry-content">
                         <?php eventbrite_ticket_form_widget(); ?>
                         <?php the_content(); ?>
